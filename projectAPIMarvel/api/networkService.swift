f6293008bdd0f069d01a757475e4b7ee55a8c6c7
levyhanunes <levyhanunes@gmail.com>
2021-05-18 22:05:34 -0400

    consumo de api buscando por name

--- projectAPIMarvel/api/networkService.swift
+++ projectAPIMarvel/api/networkService.swift
@@ -1,40 +1,81 @@
 //
-//  networkService.swift
-//  projectAPIMarvel
+//  NetworkService.swift
+//  iMarvel
 //
-//  Created by user195088 on 5/13/21.
+//  Created by user195088 on 5/15/21.
 //
 
 import UIKit
 import Alamofire
 import CryptoKit
 
-class ApiServicee {
+class ApiService {
+    
+    
     public static let get = HTTPMethod(rawValue: "GET")
-    let baseURL: String = "http://gateway.marvel.com/v1/public/characters"
+    
+    
     let privateKey = "458e3dedc56e090294f35a8c16abfa63bee16b11"
     let publicKey = "d9170f7cc7317a4f78fcc0323c3c7d15"
     let ts = String(Date().timeIntervalSince1970)
     
-    
-    public func apiRequest() {
+    // MARK: - Reques
+    // MARK: - Request2
+    public func apiRequest2(name : String) {
         
         let parameters = ["ts": ts, "hash" : self.getMD5(),  "apikey" : publicKey]
-        
+        let baseURL: String = "http://gateway.marvel.com/v1/public/characters?name=\(name)"
         let request = AF.request(baseURL, parameters: parameters)
-        // 2
-        request.responseJSON { (data) in
-            print(data)
+        request.responseJSON { (data) in print(data)
+        
             
         }
     }
     
-    private func getMD5() -> String {
+    
+    // MARK: - Search Request
+    
+    static let shared = ApiService()
+    
+  
+    
+    public func buscaCharacters2(texto: String, completion: @escaping (ReturnApi?, Error?) -> ()){
+        
+        guard let url = URL(string: "http://gateway.marvel.com/v1/public/characters?ts=\(ts)&apikey=\(publicKey)&hash=\(getMD5())&nameStartsWith=\(texto)")  else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            if let err = err {
+                completion(nil, err)
+                return
+            }
+            
+            do {
+                guard let data = data else {return}
+                let characters = try JSONDecoder().decode(ReturnApi.self, from: data)
+                completion(characters, nil)
+                
+            } catch let err {
+                completion(nil, err)
+                return
+            }
+        }.resume()
+    }
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - MD5
+         func getMD5() -> String {
+            
         let apiData = ts + privateKey + publicKey
         guard let data = apiData.data(using: .utf8) else { return String() }
         let apiHash = Insecure.MD5.hash(data: data)
         return apiHash.map { String(format: "%02hhx", $0) }.joined()
     }
-    
 }
+
+
 
--- projectAPIMarvel/model/SearchCharacters.swift
+++ projectAPIMarvel/model/SearchCharacters.swift
@@ -1,25 +1,56 @@
-//
-//  SearchResults.swift
-//  projectAPIMarvel
-//
-//  Created by user195088 on 5/13/21.
-//
+import Foundation
 
-import UIKit
+ // MARK: - ReturnApi
+ struct ReturnApi: Codable {
+     let copyright, attributionText: String
+     let code : Int
+     let attributionHTML: String
+     let data: DataClass
+     let etag: String
+ }
 
-struct  APIResults : Codable{
-    var data: APICharacterData
+ // MARK: - DataClass
+ struct DataClass: Codable {
+     let offset, limit, total, count: Int
+     let results: [Result]
+ }
+
+ // MARK: - Result
+ struct Result: Codable {
+    let id: Int
+    let name: String
+    let resourceURI : String
+    let description: String
+    let thumbnail: Thumbnail
+    
+    enum CodingKeys: String, CodingKey{
+        case id
+        case name
+        case resourceURI
+        case thumbnail
+        case description
+ }
+
+
+// MARK: -Heros
+struct Heros: Codable{
+
+    }
 }
 
-struct APICharacterData : Codable {
-    var count: Int
-    var results: [Character]
-}
-
-struct Character : Identifiable, Codable{
-    var id: Int
-    var name: String
-    var description: String
-    var thumbnail: [String:String]
-    var urls: [String:String]
+struct Thumbnail: Codable {
+    let path: String
+    let xtension : String
+    
+    var url: String {
+        return "\(path).\(xtension)"
+    }
+    
+    enum CodingKeys: String, CodingKey{
+        case xtension = "extension"
+        case path
+    }
+    
+    
+    
 }
