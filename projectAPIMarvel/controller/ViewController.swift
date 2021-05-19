093afc716243c5a7a55df3c2986ebe8d7f7ec83a
Vitor <Vito.r.natal@hotmail.com>
2021-05-18 23:18:11 -0400

    Successfull populate TableView

--- projectAPIMarvel/controller/ViewController.swift
+++ projectAPIMarvel/controller/ViewController.swift
@@ -7,43 +7,72 @@
 
 import UIKit
 import CryptoKit
-class ViewController: UIViewController{
+import Alamofire
+
+class ViewController: UIViewController, UITableViewDelegate{
     @IBOutlet weak var tableView: UITableView!
-    var apiRequest = ApiService()
-    
+    private var apiService = ApiService()
     override func viewDidLoad() {
         super.viewDidLoad()
-        apiRequest.delegate = self
+        tableView.delegate = self
         tableView.dataSource = self
-        apiRequest.apiRequest(id: 1009368)
-
-       
+        apiService.delegate = self
+        apiService.apiRequest(id: 1)
     }
 }
 
- // MARK: - EXTENSION
-extension ViewController: ApiResponse, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        apiRequest.charactersResponse!.data.results.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CellModel", for: indexPath)
-        cell.textLabel?.text  = apiRequest.charactersResponse?.data.results[indexPath.row].name
-        return cell
-    }
-    
 
+
+extension ViewController :UITableViewDataSource, ApiResponse
+{
     func success() {
-        
+        tableView.reloadData()
     }
     
     func error() {
         print("Error")
     }
     
-
-
-       
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return apiService.character.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellTableView")
+        if (apiService.character.count > 0){
+            do{
+                cell?.textLabel?.text = apiService.character[indexPath.row].name
+        }}
+        return cell!
+    }
+    
     
 }
+
+//
+// // MARK: - EXTENSION
+//extension ViewController: ApiResponse, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        apiRequest.charactersResponse!.data.results.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "CellModel", for: indexPath)
+//        cell.textLabel?.text  = apiRequest.charactersResponse?.data.results[indexPath.row].name
+//        return cell
+//    }
+//
+//
+//    func success() {
+//
+//    }
+//
+//    func error() {
+//        print("Error")
+//    }
+//
+//
+//
+//
+//
+//}
